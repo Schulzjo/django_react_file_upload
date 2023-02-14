d6c38c8087765b7dcb9ba088efbd10c2cef68f3e
schulzjo <schulzeeverding.johannes@gmail.com>
2023-02-14 14:20:06 +0100

    add initial integration test of ExcelImportView

---
+++ backend/excel_import/tests/test_excel_import_view.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import pytest
+from excel_import.models import ImportedData
+
+
+def test_excel_import_view_no_file_status_400(api_client):
+    client = api_client()
+    endpoint = '/import_excel/'
+
+    response = client.post(path=endpoint)
+
+    assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_excel_import_view_with_csv_status_200(api_client):
+    client = api_client()
+    endpoint = '/import_excel/'
+
+    with open("test_csv.csv", "r") as file:
+        response = client.post(endpoint, {'file': file}, )
+        assert response.status_code == 200
+    df = pd.read_csv("test_csv.csv")
+
+    imported_data = ImportedData.objects.all()
+
+    assert len(df) == imported_data.count()
+
+# TODO: test with excel file
