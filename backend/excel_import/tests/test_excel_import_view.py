c86f04ee03af8e54163b166e80bb60b0db426120
schulzjo <schulzeeverding.johannes@gmail.com>
2023-02-14 17:23:41 +0100

    add test for excel file

--- backend/excel_import/tests/test_excel_import_view.py
+++ backend/excel_import/tests/test_excel_import_view.py
@@ -26,4 +26,18 @@ def test_excel_import_view_with_csv_status_200(api_client):
 
     assert len(df) == imported_data.count()
 
-# TODO: test with excel file
+
+@pytest.mark.django_db
+def test_excel_import_view_with_xlsx_status_200(api_client):
+    client = api_client()
+    endpoint = '/import_excel/'
+
+    with open("test_excel.xlsx", "rb") as file:
+        response = client.post(endpoint, {'file': file}, )
+        assert response.status_code == 200
+    df = pd.read_excel("test_excel.xlsx")
+
+    imported_data = ImportedData.objects.all()
+
+    assert len(df) == imported_data.count()
+
